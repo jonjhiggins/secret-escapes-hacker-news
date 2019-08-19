48f38d22c4d99e77774f681d534255393a8fe413
Jon Higgins <jon@jonhiggins.co.uk>
2019-08-19 10:40:25 +0100

    story - active state

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Story from "../Story";
 import "./index.css";
 
 export default function Stories({ stories }) {
+  const [activeStoryIndex, setActiveStoryIndex] = useState(-1);
+
+  /**
+   * Set a story to be active, set others to be inactive
+   * @param {number} storyIndex
+   */
+  const setActiveStory = storyIndex => {
+    setActiveStoryIndex(activeStoryIndex => {
+      // If clicking an active story toggle it to be inactive,
+      // otherwise set the active story index
+      return storyIndex !== activeStoryIndex ? storyIndex : -1;
+    });
+  };
+
   return (
     <div className="stories">
       {" "}
       {stories.map((story, index) => (
-        <Story story={story} key={index} />
+        <Story
+          story={story}
+          setActiveStory={setActiveStory}
+          key={index}
+          active={activeStoryIndex === index}
+          index={index}
+        />
       ))}
     </div>
   );
--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./index.css";
 
-export default function Story({ story }) {
+export default function Story({ story, setActiveStory, active, index }) {
   const { title, author, text, score } = story;
 
   /**
@@ -13,15 +13,31 @@ export default function Story({ story }) {
    */
   const getExcept = text => `${text.replace(/^(.{200}[^\s]*).*/, "$1")}...`;
 
+  /**
+   * @TODO upvote functionality
+   */
+  const upvote = () => alert("Upvote");
+
+  /**
+   * Only set active story on hitting enter when using keyboard nav
+   * @param {Event} event
+   */
+  const handleKeyDown = event => event.keyCode !== 13 || setActiveStory(index);
   return (
-    <article className="story">
-      <div className="story-main">
+    <article className={`story ${active ? "is-active" : ""}`}>
+      <div
+        className="story-main"
+        role="button"
+        onClick={() => setActiveStory(index)}
+        onKeyDown={handleKeyDown}
+        tabIndex="0"
+      >
         <h2 className="story-title">{title}</h2>
         <p className="story-author">{author}</p>
-        <div dangerouslySetInnerHTML={{ __html: getExcept(text) }} />
+        <div dangerouslySetInnerHTML={{ __html: active ? text : getExcept(text) }} />
       </div>
       <div className="story-extra">
-        <button type="button" className="story-upvote">
+        <button type="button" className="story-upvote" onClick={upvote}>
           <span className="story-upvote-arrow" />
           <span className="story-upvote-text">Upvote</span>
         </button>
